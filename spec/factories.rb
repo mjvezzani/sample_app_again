679ca60948194cf1f09c9518665cfaed49110ec6
Michael Vezzani <mjvezzani@gmail.com>
2013-04-24 10:55:07 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Michael Vezzani"
+    email     "mjvezzani@gmail.com"
+    password  "jimmyjohn"
+    password_confirmation "jimmyjohn"
+  end
+end
